f0714b8bc2e9751eadc6f99d6c27ffa8e7a6aa6c
joao <joao.camargo@gmail.com>
2021-08-22 09:55:58 -0300

    refatorando

--- EitaCommerceCore/EitaCommerceCoreTests/CartTests.swift
+++ EitaCommerceCore/EitaCommerceCoreTests/CartTests.swift
@@ -6,35 +6,7 @@
 //
 
 import XCTest
-
-class Cart {
-    
-    private(set) var items: [String]
-    
-   
-    init(items: [String]) {
-        self.items = items
-    }
-    
-    func addItem(_ item: String) {
-        items.append(item)
-    }
-    
-    func removeItem(_ item: String) {
-        
-        let index = items.firstIndex(where: { $0 == item }) // OR  items.firstIndex(of: Iitem)
-        
-        if let index = index {
-            items.remove(at: index)
-        }
-    }
-    
-    func clear() {
-        items.removeAll()
-    }
-    
-    
-}
+import EitaCommerceCore
 
 class CartTests: XCTestCase {
     
@@ -51,8 +23,8 @@ class CartTests: XCTestCase {
         sut.addItem(item1)
                
         //Assert
-        XCTAssertEqual(sut.items.count,1)
-        XCTAssertEqual(sut.items.first,item1)
+        XCTAssertEqual(sut.getItems().count,1)
+        XCTAssertEqual(sut.getItems().first,item1)
     }
     
     
@@ -66,9 +38,9 @@ class CartTests: XCTestCase {
         sut.addItem(item2)
                
         //Assert
-        XCTAssertEqual(sut.items.first,item1)
-        XCTAssertEqual(sut.items.last,item2)
-        XCTAssertEqual(sut.items.count,2)
+        XCTAssertEqual(sut.getItems().first,item1)
+        XCTAssertEqual(sut.getItems().last,item2)
+        XCTAssertEqual(sut.getItems().count,2)
     }
     
     func testCart_RemoveOneIitem_shouldHaveOneIitem(){
@@ -79,9 +51,9 @@ class CartTests: XCTestCase {
         sut.removeItem(item1)
         
         //Assert
-        XCTAssertEqual(sut.items.first,item2)
-        XCTAssertEqual(sut.items.last,item2)
-        XCTAssertEqual(sut.items.count,1)
+        XCTAssertEqual(sut.getItems().first,item2)
+        XCTAssertEqual(sut.getItems().last,item2)
+        XCTAssertEqual(sut.getItems().count,1)
     }
     
     func testCart_RemoveOneIitem_shouldHaveTwoIitem(){
@@ -93,9 +65,9 @@ class CartTests: XCTestCase {
 
                
         //Assert
-        XCTAssertEqual(sut.items.first,item1)
-        XCTAssertEqual(sut.items.last,item2)
-        XCTAssertEqual(sut.items.count,2)
+        XCTAssertEqual(sut.getItems().first,item1)
+        XCTAssertEqual(sut.getItems().last,item2)
+        XCTAssertEqual(sut.getItems().count,2)
     }
     
     func testCard_clearAllCart_CartShouldBeEmpty(){
@@ -104,7 +76,7 @@ class CartTests: XCTestCase {
         
         sut.clear()
         
-        XCTAssertTrue(sut.items.isEmpty)
+        XCTAssertTrue(sut.getItems().isEmpty)
     }
     
 }
